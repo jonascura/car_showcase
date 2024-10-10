6c80ff3ebe64c389224968ff9870505757c71fa5
Jonas Cura <jonasm.cura@gmail.com>
2024-10-10 13:24:55 -0600

    add nav and footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,6 +23,7 @@ const Hero = () => {
 
         <CustomButton 
           title="Explore Cars"
+          btnType="button"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
@@ -31,8 +32,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay" />
         </div>
+        <div className="hero__image-overlay" />
 
       </div>
     </div>
--- types/index.ts
+++ types/index.ts
@@ -5,4 +5,5 @@ export interface CustomButtonProps {
   containerStyles?: string; // questionmark = optional
   handleClick?:
   MouseEventHandler<HTMLButtonElement>
+  btnType: "button" | "submit"
 }
